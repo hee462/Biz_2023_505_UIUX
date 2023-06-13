85f02561b0f9baede31fcd3dc6cc9d0a43a0f2d8
hee462 <a01080809887@gmail.com>
2023-06-13 16:18:03 +0900

    json

---
+++ js_02_json/json3.js
@@ -0,0 +1,4 @@
+// JS 에서 AJAX 방식으로 BackEnd Server로 부터 데이터를 받아오는 함수
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
