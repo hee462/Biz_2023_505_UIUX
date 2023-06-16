10d516fb0a191c14fddc4be3bc4da7375e7c03fa
hee462 <a01080809887@gmail.com>
2023-06-16 14:52:01 +0900

    js

--- GodLife/recommend.js
+++ GodLife/recommend.js
@@ -1,1 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  function setChallenge() {
+    const element = document.getElementById("challenge_water");
+    alert(element);
+    // element.innerHTML = "<div>물마시기</div>";
+  }
+});
+// const box_add = function () {};
+
+// $(document).ready(function () {
+//   var i = 1; // 변수설정은 함수의 바깥에 설정!
+//   $("button").click(function (event) {
+//     alert("!");
+//     // $("#box_add").append("<button class='item'>등장\"+i+\"</button>");
+
+//     i++; // 함수 내 하단에 증가문 설정
+//   });
+// });
